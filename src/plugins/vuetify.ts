3d76380f0c0ee785a6f49f1db65158763bfa4760
Akos Szalay <akos.szalay@gmail.com>
2020-10-20 17:56:46 +0100

    vuetify + i18n setup

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,26 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import VueI18n from "vue-i18n";
+import en from "vuetify/src/locale/en";
+import hu from "vuetify/src/locale/hu";
 
 Vue.use(Vuetify);
+Vue.use(VueI18n);
 
-export default new Vuetify({});
+const messages = {
+  en: { $vuetify: en },
+  hu: { $vuetify: hu }
+};
+
+const i18n = new VueI18n({
+  locale: "en", // set locale
+  messages // set locale messages
+});
+
+export default new Vuetify({
+  lang: {
+    t: (key: string, ...params: unknown[]) => i18n.t(key, params) as string,
+    locales: { en, hu },
+    current: "en"
+  }
+});
