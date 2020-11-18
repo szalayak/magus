a685cba32ee5f729fd5f6b8536ef2ec25511a1e9
Akos Szalay <akos.szalay@gmail.com>
2020-11-18 08:54:44 +0000

    temporarily replace home view with player characters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,44 @@
 <template>
-  <character-list
+  <player-characters-as-player />
+  <!-- <character-list
     :characters="characters"
     :messages="messages"
     :notification="notification"
     :title="$t('my-characters')"
-  />
+  /> -->
 </template>
 
 <script lang="ts">
 import { Component } from "vue-property-decorator";
-import { GraphQLResult } from "@aws-amplify/api-graphql";
-import CharacterList from "@/components/CharacterList.vue";
-import { Character } from "@/store/modules/character";
+// import { GraphQLResult } from "@aws-amplify/api-graphql";
+// import CharacterList from "@/components/CharacterList.vue";
+// import { Character } from "@/store/modules/character";
 import TitleComponent from "@/mixins/TitleComponent";
 import { mixins } from "vue-class-component";
+import PlayerCharactersAsPlayer from "./character/PlayerCharactersAsPlayer.vue";
 
 @Component({
   components: {
-    "character-list": CharacterList,
+    // "character-list": CharacterList,
+    "player-characters-as-player": PlayerCharactersAsPlayer,
   },
 })
 export default class Home extends mixins(TitleComponent) {
-  messages: string[] = [];
-  notification = false;
+  // messages: string[] = [];
+  // notification = false;
   title = this.$t("home");
 
-  get characters(): Character[] {
-    return this.$store.getters["character/playerCharactersAsPlayer"];
-  }
+  // get characters(): Character[] {
+  //   return this.$store.getters["character/playerCharactersAsPlayer"];
+  // }
 
-  created() {
-    this.$store
-      .dispatch("character/loadByOwner", this.$store.state.app.user.username)
-      .catch((error: GraphQLResult<Character>) => {
-        this.messages = error.errors?.map(err => err.message) || [];
-        this.notification = true;
-      });
-  }
+  // created() {
+  //   this.$store
+  //     .dispatch("character/loadByOwner", this.$store.state.app.user.username)
+  //     .catch((error: GraphQLResult<Character>) => {
+  //       this.messages = error.errors?.map(err => err.message) || [];
+  //       this.notification = true;
+  //     });
+  // }
 }
 </script>
