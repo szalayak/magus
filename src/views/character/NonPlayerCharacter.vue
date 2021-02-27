bab324dc583f93cf58c57407143a411833fb9987
Akos Szalay <akos.szalay@gmail.com>
2021-02-27 17:29:18 +0000

    fixed npc page

--- src/views/character/NonPlayerCharacter.vue
+++ src/views/character/NonPlayerCharacter.vue
@@ -4,130 +4,132 @@
     :notification="notification"
     :messages="messages"
     @refresh="refresh"
+    v-if="character"
+    :loading="loading"
   >
-    <character-overview v-if="page === 0" :id="id" :character="character" />
-    <v-row>
-      <v-col v-if="page === 1" cols="12">
+    <v-row dense no-gutters>
+      <v-col id="appearance" cols="12">
         <appearance :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 1" cols="12" xs="12" sm="6" lg="4">
+      <v-col id="administrative-info" cols="12">
+        <administrative-info
+          :id="id"
+          :character="character"
+          :editable="editable"
+        />
+      </v-col>
+      <v-col id="properties" cols="12">
         <properties :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 1" cols="12" xs="12" sm="6" lg="4">
+      <v-col id="likes-and-dislikes" cols="12">
         <likes-and-dislikes
           :id="id"
           :editable="editable"
           :character="character"
         />
       </v-col>
-      <v-col v-if="page === 1" cols="12" xs="12" sm="6" lg="4">
+      <v-col id="connections" cols="12">
         <connections :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 2" cols="12">
+      <v-col id="core-information" cols="12">
         <core-information
           :id="id"
           :editable="editable"
           :character="character"
         />
       </v-col>
-      <v-col v-if="page === 2" cols="12" xs="12" sm="6" lg="4" xl="2">
+      <v-col id="abilities" cols="12">
         <abilities :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 2" cols="12" xs="12" sm="6" lg="4" xl="2">
+      <v-col id="health" cols="12">
         <health :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 2" cols="12" xs="12" sm="6" lg="4" xl="2">
+      <v-col id="combat-values" cols="12">
         <combat-values :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 2" cols="12" xs="12" sm="6" lg="4" xl="2">
+      <v-col id="spell-resistance" cols="12">
         <spell-resistance
           :id="id"
           :editable="editable"
           :character="character"
         />
       </v-col>
-      <v-col
-        v-if="page === 2 && character.psiUser"
-        cols="12"
-        xs="12"
-        sm="6"
-        lg="2"
-      >
+      <v-col id="psi" v-if="character.psiUser">
         <psi :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col
-        v-if="page === 2 && character.magicUser"
-        cols="12"
-        xs="12"
-        sm="3"
-        lg="2"
-      >
+      <v-col id="magical-ability" v-if="character.magicUser" cols="12">
         <magical-ability :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 2" cols="12" xs="12" sm="3" lg="2">
+      <v-col id="armour" cols="12">
         <armour :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 2" cols="12" xs="12" sm="3" lg="2">
+      <v-col id="shield" cols="12">
         <shield :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 3" cols="12">
+      <v-col id="weapon-assignments" cols="12">
         <weapon-assignments
           :id="id"
           :editable="editable"
           :character="character"
         />
       </v-col>
-      <v-col v-if="page === 3" cols="12">
+      <v-col id="ranged-weapon-assignments" cols="12">
         <ranged-weapon-assignments
           :id="id"
           :editable="editable"
           :character="character"
         />
       </v-col>
-      <v-col v-if="page === 3" cols="12">
+      <v-col id="skill-assignments" cols="12">
         <skill-assignments
           :id="id"
           :editable="editable"
           :character="character"
         />
       </v-col>
-      <v-col v-if="page === 4" cols="12" xs="12" sm="6" md="4">
+      <v-col id="skill-points" cols="12">
+        <skill-points :id="id" :editable="editable" :character="character" />
+      </v-col>
+      <v-col id="magical-item-assignments" cols="12">
         <magical-item-assignments
           :id="id"
           :editable="editable"
           :character="character"
         />
       </v-col>
-      <v-col v-if="page === 4" cols="12" xs="12" sm="6" md="4">
-        <wallet :id="id" :editable="editable" :character="character" />
-      </v-col>
-      <v-col v-if="page === 4" cols="12" xs="12" sm="6" md="4">
-        <inventory :id="id" :editable="editable" :character="character" />
-      </v-col>
-      <v-col v-if="page === 4" cols="12">
-        <animals :id="id" :editable="editable" :character="character" />
-      </v-col>
-      <v-col v-if="page === 4" cols="12">
-        <servants :id="id" :editable="editable" :character="character" />
-      </v-col>
-      <v-col v-if="page === 4" cols="12" xs="12" sm="6">
+      <v-col id="languages" cols="12">
         <languages :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 4" cols="12" xs="12" sm="6">
+      <v-col id="wallet" cols="12">
+        <wallet :id="id" :editable="editable" :character="character" />
+      </v-col>
+      <v-col id="inventory" cols="12">
+        <inventory :id="id" :editable="editable" :character="character" />
+      </v-col>
+      <v-col id="poisons" cols="12">
         <poisons :id="id" :editable="editable" :character="character" />
       </v-col>
-      <v-col v-if="page === 4" cols="12">
+      <v-col id="animals" cols="12">
+        <animals :id="id" :editable="editable" :character="character" />
+      </v-col>
+      <v-col id="servants" cols="12">
+        <servants :id="id" :editable="editable" :character="character" />
+      </v-col>
+      <v-col id="notes" cols="12">
         <notes :id="id" :editable="editable" :character="character" />
       </v-col>
     </v-row>
   </character-page-layout>
+  <skeleton-cards v-else :count="12" />
 </template>
 <script lang="ts">
+import CharacterPage from "./CharacterPage";
 import Component from "vue-class-component";
 import AppearanceCard from "@/components/character/AppearanceCard.vue";
 import PropertiesCard from "@/components/character/PropertiesCard.vue";
 import LikesAndDislikesCard from "@/components/character/LikesAndDislikesCard.vue";
 import ConnectionsCard from "@/components/character/ConnectionsCard.vue";
+import AdministrativeInfoCard from "@/components/character/AdministrativeInfoCard.vue";
 import CoreInformationCard from "@/components/character/CoreInformationCard.vue";
 import AbilitiesCard from "@/components/character/AbilitiesCard.vue";
 import HealthCard from "@/components/character/HealthCard.vue";
@@ -140,6 +142,7 @@ import ShieldCard from "@/components/character/ShieldCard.vue";
 import WeaponAssignmentCard from "@/components/character/WeaponAssignmentCard.vue";
 import RangedWeaponAssignmentCard from "@/components/character/RangedWeaponAssignmentCard.vue";
 import SkillAssignmentCard from "@/components/character/SkillAssignmentCard.vue";
+import SkillPointsCard from "@/components/character/SkillPointsCard.vue";
 import LanguageCard from "@/components/character/LanguageCard.vue";
 import WalletCard from "@/components/character/WalletCard.vue";
 import NotesCard from "@/components/character/NotesCard.vue";
@@ -148,9 +151,8 @@ import PoisonCard from "@/components/character/PoisonCard.vue";
 import MagicalItemAssignmentCard from "@/components/character/MagicalItemCard.vue";
 import AnimalsCard from "@/components/character/AnimalsCard.vue";
 import ServantsCard from "@/components/character/ServantsCard.vue";
-import CharacterPage from "./CharacterPage";
-import CharacterOverview from "@/components/character/CharacterOverview.vue";
 import CharacterPageLayout from "@/components/CharacterPageLayout.vue";
+import SkeletonCards from "@/components/SkeletonCards.vue";
 
 @Component({
   name: "non-player-character",
@@ -159,6 +161,7 @@ import CharacterPageLayout from "@/components/CharacterPageLayout.vue";
     properties: PropertiesCard,
     "likes-and-dislikes": LikesAndDislikesCard,
     connections: ConnectionsCard,
+    "administrative-info": AdministrativeInfoCard,
     "core-information": CoreInformationCard,
     abilities: AbilitiesCard,
     health: HealthCard,
@@ -171,6 +174,7 @@ import CharacterPageLayout from "@/components/CharacterPageLayout.vue";
     "weapon-assignments": WeaponAssignmentCard,
     "ranged-weapon-assignments": RangedWeaponAssignmentCard,
     "skill-assignments": SkillAssignmentCard,
+    "skill-points": SkillPointsCard,
     languages: LanguageCard,
     wallet: WalletCard,
     notes: NotesCard,
@@ -179,8 +183,8 @@ import CharacterPageLayout from "@/components/CharacterPageLayout.vue";
     "magical-item-assignments": MagicalItemAssignmentCard,
     animals: AnimalsCard,
     servants: ServantsCard,
-    "character-overview": CharacterOverview,
     "character-page-layout": CharacterPageLayout,
+    "skeleton-cards": SkeletonCards,
   },
 })
 export default class NonPlayerCharacter extends CharacterPage {}
